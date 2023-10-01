0b4c78b7f5a3403be6911c943849e3acfbb665b4
Atharva Pandharikar <atharvapandharikar5@gmail.com>
2023-10-01 18:26:04 +0530

    Second Commit

--- src/Components/MainSection.jsx
+++ src/Components/MainSection.jsx
@@ -2,9 +2,7 @@ import React, {useState} from "react";
 
 const MainSection = () => {
 
-
-
-    const [text, setText] = useState('Enter Text here2');
+    const [text, setText] = useState('lorem100');
 
     const handleUpClick = () =>{
         let newtext = text.toUpperCase();
@@ -17,12 +15,17 @@ const MainSection = () => {
     }
 
     const handleOnChange = (event) =>{
-        setText(event.target.value)
+        setText(event.target.value);
+    }
+
+    const handleClearClick = () =>{
+      let newtext = "";
+      setText(newtext);
     }
 
   return (
     <>
-      <div class="mb-3 mx-5 my-4">
+      <div class="container mb-3 mx-5 my-4">
         <label for="exampleFormControlTextarea1" class="form-label">
           Enter Your Text : 
         </label>
@@ -33,10 +36,19 @@ const MainSection = () => {
         rows="3"
         placeholder="write something...."
         onChange={handleOnChange}
-        
         ></textarea>
+
         <button className="btn btn-primary my-3" onClick={handleUpClick}>UpperCase</button>
         <button className="btn btn-primary my-3 mx-2" onClick={handleLoClick}>LowerCase</button>
+        <button className="btn btn-primary my-3 " onClick={handleClearClick}>Clear Text</button>
+
+      </div>
+      <div className="container">
+        <h4>Text Summary: </h4>
+        <p>{text.split(" ").length} words and {text.length} characters </p>
+        <p>{0.008 * text.split(" ").length} Minutes to read document..</p>
+        <h4>Preview</h4>
+        <label htmlFor="">{text}</label>
       </div>
     </>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
-            Navbar
+            TextEditor
           </a>
           <button
             class="navbar-toggler"
@@ -26,23 +26,7 @@ const Navbar = () => {
                   Home
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Link
-                </a>
-              </li>
             </ul>
-            <form class="d-flex" role="search">
-              <input
-                class="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button class="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
           </div>
         </div>
       </nav>
